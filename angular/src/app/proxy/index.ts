aa4ac6862beaf5f3b1dc50803dc6e7424e318f16
Phan Nguyen Minh Phat <phannguyenminhphat1@gmail.com>
2026-05-23 11:46:08 +0700

    Feat: Generate Angular Proxies

---
+++ angular/src/app/proxy/index.ts
@@ -0,0 +1,5 @@
+import * as MyEcommerce from './my-ecommerce';
+import * as ProductCategories from './product-categories';
+import * as Products from './products';
+export * from './models';
+export { MyEcommerce, ProductCategories, Products };
